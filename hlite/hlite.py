18c5edc9d9b2e279cc3762e80d08fab77519c252
Amlesh Sivanantham <samlesh@gmail.com>
2016-07-19 13:19:06 -0700

    Making a script that will allow to translate assembly code very easily and test it out. Useful for debuggin your code before programming into minecraft

---
+++ hlite/hlite.py
@@ -0,0 +1,17 @@
+#print("Hello World")
+
+"""
+Global variables that
+control the way the program operates.
+"""
+
+# Option to display or supress the output.
+# Defaults to supressing
+verbose=False
+
+# Option to toggle between compiling
+# or simulating.
+# Defaults to compiling=(False)
+compSim = False
+
+
