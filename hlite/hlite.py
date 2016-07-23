fbeef8012ea3f296ede841b8f78bb711e0b60f1c
Amlesh Sivanantham <samlesh@gmail.com>
2016-07-22 20:40:52 -0700

    Added the variable table and proper label table functionality

--- hlite/hlite.py
+++ hlite/hlite.py
@@ -1,13 +1,13 @@
-# -----------------------------------------------------------------	# 
-# 		HLITE - HummusLite Compiler and Simulator					#
-# 																	#
-#	The goal of this program is to convert hummuslite assembly		#
-#	into binary strings. This program also has the capability		#
-#	to run these binary strings and simulate its behaviour			#
-#	as it would on the minecraft world.								#
-#																	#
-#																	#
-# -----------------------------------------------------------------	#
+# -----------------------------------------------------------------	 
+# 		HLITE - HummusLite Compiler and Simulator					
+# 																	
+#	The goal of this program is to convert hummuslite assembly		
+#	into binary strings. This program also has the capability		
+#	to run these binary strings and simulate its behaviour			
+#	as it would on the minecraft world.								
+#																	
+#																	
+# -----------------------------------------------------------------	
 
 import sys
 
@@ -32,11 +32,12 @@ def vprint(strTemp):
 		print(strTemp)
 
 
-# Option to toggle between compiling
-# or simulating.
-# Defaults to compiling=(False)
+# Option to state that it is compiling
 
-COMPSIM = False
+ASSEMBLE = False
+
+# Option to state that it is simulating
+SIMULATE = False
 
 
 # Label Table data structure
@@ -45,23 +46,184 @@ COMPSIM = False
 
 LABEL_TABLE = {}
 
+# Variable Table data structure
+# keeps track of what variables exist
+# and what their memory address is
+
+VAR_TABLE = {}
+
 # function to generate label dictionary
 
 def createLabelTable():
 	vprint("\nGENERATE LABEL TABLE: \n")
-	curLine = -1	
+	
 	for key in FILE_DATA:
-		curLine += 1
 		try:
-			LABEL_TABLE[FILE_DATA[key][2]] = curLine
+			LABEL_TABLE[FILE_DATA[key][2]] = key
 
 		except IndexError:
-			vprint("Line "+str(curLine)+": Label not found")
-			continue
+			vprint("Line "+str(key)+": Label not found")
 
 		else:
-			vprint("Line "+str(curLine)+": Label found ---> "+FILE_DATA[key][2])
-			continue
+			vprint("Line "+str(key)+": Label found ---> "+FILE_DATA[key][2])
+	
+	vprint("\n--------------------------------------------\n*\t\tLABEL TABLE\n*")
+	
+	for labelName in LABEL_TABLE:
+		vprint("*\t"+labelName+": \t\t"+str(LABEL_TABLE[labelName])) 
+
+	vprint("*\n--------------------------------------------\n")
+
+
+
+# function to generate variable dictionary
+# Also converts data in argument field to
+# integer if its not a variable
+#
+# It also sorts out the information of other
+# arguments while they are at it.
+
+def createVarTable():
+	vprint("\nDECODE ARGUMENTS: \n")
+	memLocation = 0
+	varNotFound = False
+
+	for key in FILE_DATA:
+		varFound = False
+
+		try:
+			FILE_DATA[key][1] = int(FILE_DATA[key][1])
+			vprint("Line "+str(key)+": Integer found ---> "+str(FILE_DATA[key][1]))
+
+		except IndexError:
+			print("\n*\t*\t*\t*\t*\nError! No argument found in Line "+str(key))
+			exit(1)
+
+		except ValueError:
+
+			# REGISTER REFERENCE B1 AND B2
+			if(FILE_DATA[key][1] == "B1"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 0
+			
+			elif(FILE_DATA[key][1] == "B2"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 1
+
+			# ADD SPECIFIC ARGUMENTS
+			elif(FILE_DATA[key][1] == "B1+B2"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 0
+			
+			elif(FILE_DATA[key][1] == "B1-B2"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 1
+			
+			elif(FILE_DATA[key][1] == "-B1+B2"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 2
+			
+			elif(FILE_DATA[key][1] == "-B1-B2"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 3
+
+			# BOOL SPECIFIC ARGUMENTS
+			elif(FILE_DATA[key][1] == "BAND"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 0
+			
+			elif(FILE_DATA[key][1] == "LAND"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 1
+			
+			elif(FILE_DATA[key][1] == "BOR"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 2
+			
+			elif(FILE_DATA[key][1] == "LOR"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 3
+			
+			elif(FILE_DATA[key][1] == "XOR"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 4
+			
+			elif(FILE_DATA[key][1] == "XNOR"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 5
+			
+			elif(FILE_DATA[key][1] == "LB1"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 6
+			
+			elif(FILE_DATA[key][1] == "LB2"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 7
+			
+			elif(FILE_DATA[key][1] == "BNAND"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 8
+			
+			elif(FILE_DATA[key][1] == "LNAND"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 9
+			
+			elif(FILE_DATA[key][1] == "BNOR"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 10
+			
+			elif(FILE_DATA[key][1] == "LNOR"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 11
+			
+			elif(FILE_DATA[key][1] == "NB1"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 12
+			
+			elif(FILE_DATA[key][1] == "NB2"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 13
+			
+			elif(FILE_DATA[key][1] == "RB1"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 14
+			
+			elif(FILE_DATA[key][1] == "RB1"):
+				vprint("Line "+str(key)+": Keyword found ---> "+str(FILE_DATA[key][1]))
+				FILE_DATA[key][1] = 15
+
+			# IF NOT KEYWORD -> THEN ITS A VARIABLE
+			else:
+				
+				# check to see if variable aldready exists in table
+				for varIndex in VAR_TABLE:
+					
+					if(VAR_TABLE[varIndex] == FILE_DATA[key][1]):
+						varFound = True
+						vprint("Line "+str(key)+": Old Variable found ---> "+FILE_DATA[key][1])
+						break
+
+				# check to see if its actuall a label
+				for labelName in LABEL_TABLE:
+					
+					if(labelName  == FILE_DATA[key][1]):
+						varFound = True
+						print("Line "+str(key)+": Label Pointer found ---> "+FILE_DATA[key][1])
+						break
+
+				# if both cases are not found, then add it to the table
+				if(not varFound):
+					vprint("Line "+str(key)+": New Variable found ---> "+FILE_DATA[key][1])
+					VAR_TABLE[memLocation] = FILE_DATA[key][1]
+					memLocation += 1
+
+
+	vprint("\n--------------------------------------------\n*\t\tVARIABLE TABLE\n*")
+	
+	for varLocation in VAR_TABLE:
+		vprint("*\t"+str(varLocation)+": "+VAR_TABLE[varLocation]) 
+
+	vprint("*\n--------------------------------------------\n")
 
 
 # function to calculate offset of current line number
@@ -98,8 +260,10 @@ except IOError:
 else:
 	curLine = 0
 	for line in humFile:
+		
 		if(line[0] == "#"):
 			continue
+		
 		FILE_DATA[curLine] = line.split()
 		vprint("Line "+str(curLine)+" ---> "+str(FILE_DATA[curLine]))
 		curLine += 1
@@ -169,6 +333,6 @@ def decodeInstruction(asmi, lineNum):
 # -------------------------------------------------------
 """
 
-
 createLabelTable()
+createVarTable()
 asmCompile()
